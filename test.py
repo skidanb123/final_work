1853525947058fa4817919765086c0b46da5d8dc
Bohdan <skidanb123@gmail.com>
2020-11-09 11:56:46 +0200

    Grouped transactions and aggregated features

--- test.py
+++ test.py
@@ -31,33 +31,36 @@ def max(x):
         return 0
 
 
-chunk0 = pd.read_csv("chunks/chunk0", comment='#', error_bad_lines=False, sep=',', index_col=0, low_memory=False)
-chunk0 = chunk0.drop(columns=['txnDescription', "txnDate"])
-print(chunk0.head())
-for col in chunk0.columns:
-    print(col)
-##############################
-grouped_sum = chunk0.groupby('advanceID').agg(
-    {"txnAmount": summ})
-grouped_sum = grouped_sum.rename(columns={'txnAmount': 'txn_sum'})
-#########################
-grouped_mean = chunk0.groupby('advanceID').agg(
-    {"txnAmount": mean})
-grouped_mean = grouped_mean.rename(columns={'txnAmount': 'txn_mean'})
-##############################
-grouped_min = chunk0.groupby('advanceID').agg(
-    {"txnAmount": min})
-grouped_min = grouped_min.rename(columns={'txnAmount': 'txn_min'})
-###############################
-grouped_max = chunk0.groupby('advanceID').agg(
-    {"txnAmount": max})
-grouped_max = grouped_max.rename(columns={'txnAmount': 'txn_max'})
-###############################
-# chunk0=chunk0.groupby('advanceID').aggregate(summ(chunk0.txnAmount))
-print("mean ", grouped_mean.head())
-print("sum ", grouped_sum.head())
-print("min ", grouped_min.head())
-print("max ", grouped_max.head())
-grouped = [grouped_max, grouped_mean, grouped_min, grouped_sum]
-df_final = reduce(lambda left, right: pd.merge(left, right, on='advanceID'), grouped)
-print(df_final.head())
+final = pd.DataFrame()
+for i in range(149):
+    if i != 1:
+        chunk0 = pd.read_csv("chunks/chunk" + str(i), comment='#', error_bad_lines=False, sep=',', index_col=0,
+                             low_memory=False)
+        chunk0 = chunk0.drop(columns=['txnDescription', "txnDate"])
+    else:continue
+    ##############################
+    grouped_sum = chunk0.groupby('advanceID').agg(
+        {"txnAmount": summ})
+    grouped_sum = grouped_sum.rename(columns={'txnAmount': 'txn_sum'})
+    #########################
+    grouped_mean = chunk0.groupby('advanceID').agg(
+        {"txnAmount": mean})
+    grouped_mean = grouped_mean.rename(columns={'txnAmount': 'txn_mean'})
+    ##############################
+    grouped_min = chunk0.groupby('advanceID').agg(
+        {"txnAmount": min})
+    grouped_min = grouped_min.rename(columns={'txnAmount': 'txn_min'})
+    ###############################
+    grouped_max = chunk0.groupby('advanceID').agg(
+        {"txnAmount": max})
+    grouped_max = grouped_max.rename(columns={'txnAmount': 'txn_max'})
+    ###############################
+    # chunk0=chunk0.groupby('advanceID').aggregate(summ(chunk0.txnAmount))
+
+    grouped = [grouped_max, grouped_mean, grouped_min, grouped_sum]
+
+    df_final = reduce(lambda left, right: pd.merge(left, right, on='advanceID'), grouped)
+    final = final.append(df_final)
+    print(i)
+final.to_csv("grouped_and_united")
+
